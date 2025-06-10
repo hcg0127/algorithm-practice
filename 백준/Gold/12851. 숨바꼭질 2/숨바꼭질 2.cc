57d3a253604ff32066a2233c6db3a171425fa95d
Eunji Kim <kej431003@gmail.com>
2025-06-10 21:08:51 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -2,40 +2,36 @@
 #define pii pair<int,int>
 using namespace std;
 
-int n,k,arr[100001]={},visited[100001]={};
+int n,k,arr[100001],visited[100001];
 
 void bfs(int x) {
   queue<int> q;
   q.push(x);
-  arr[x]=0;
+  arr[x]=1;
   visited[x]=1;
   while (!q.empty()) {
-    int cur = q.front(),dx[3]={cur-1,cur+1,cur*2};
+    int cur = q.front();
     q.pop();
-    for (int i=0; i<3; i++) {
-      if (dx[i]<0 || dx[i]>100000) continue;
-      if (arr[dx[i]]!=-1) {
-        if (arr[dx[i]]==arr[cur]+1) visited[dx[i]]+=visited[cur];
-      }
-      else {
-        q.push(dx[i]);
-        visited[dx[i]]=visited[cur];
-        arr[dx[i]]=arr[cur]+1;
-      }
+    for (auto i : {cur-1,cur+1,cur*2}) {
+        if (i<0 || i>100000) continue;
+        if (!arr[i]) {
+            q.push(i);
+            arr[i]=arr[cur]+1;
+            visited[i]+=visited[cur];
+        } else if (arr[i]==arr[cur]+1) visited[i]+=visited[cur];
     }
   }
 }
 
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-  cin >> n >> k;
-  fill(arr,arr+100001,-1);
-  if (n==k) cout << 0 << '\n' << 1;
-  else if (n>k) cout << n-k << '\n' << 1;
-  else {
+    cin >> n >> k;
+    if (n==k) {
+        cout << 0 << '\n' << 1;
+        return 0;
+    }
     bfs(n);
-    cout << arr[k] << '\n' << visited[k];
-  }
+    cout << arr[k]-1 << '\n' << visited[k];
 }

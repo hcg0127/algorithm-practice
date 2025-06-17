4868a788ad4463f7d5a60467a7c3427a7c42d8d7
Eunji Kim <kej431003@gmail.com>
2025-06-17 18:22:48 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 2376 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -2,21 +2,25 @@
 #define pii pair<int,int>
 using namespace std;
 
-int n,arr[3],mu[6][3]={{9,3,1},{9,1,3},{3,9,1},{3,1,9},{1,9,3},{1,3,9}},visited[61][61][61];
+int n,arr[3],mu[6][3]={{9,3,1},{9,1,3},{3,9,1},{3,1,9},{1,9,3},{1,3,9}},ans=1e9;
+bool visited[61][61][61];
 
 void bfs() {
-    queue<pair<pii,int>> q;
-    q.push({{arr[0],arr[1]},arr[2]});
+    queue<pair<pii,pii>> q;
+    q.push({{arr[0],arr[1]},{arr[2],0}});
     visited[arr[0]][arr[1]][arr[2]]=1;
     while(!q.empty()) {
         auto [a,b] = q.front();
         q.pop();
-        if (visited[0][0][0]) break;
         for (int i=0; i<6; i++) {
-            int cx=max(0,a.first-mu[i][0]),cy=max(0,a.second-mu[i][1]),cz=max(0,b-mu[i][2]);
+            int cx=max(0,a.first-mu[i][0]),cy=max(0,a.second-mu[i][1]),cz=max(0,b.first-mu[i][2]);
             if (visited[cx][cy][cz]) continue;
-            q.push({{cx,cy},cz});
-            visited[cx][cy][cz]=visited[a.first][a.second][b]+1;
+            if (cx==0 && cy==0 && cz==0) {
+                ans = b.second+1;
+                return;
+            }
+            q.push({{cx,cy},{cz,b.second+1}});
+            visited[cx][cy][cz]=1;
         }
     }
 }
@@ -28,5 +32,5 @@ int main() {
     cin >> n;
     for (int i=0; i<n; i++) cin >> arr[i];
     bfs();
-    cout << visited[0][0][0]-1;
+    cout << ans;
 }

471b72147fadbb4419725688c321eb2299cd1892
Eunji Kim <kej431003@gmail.com>
2025-10-29 19:19:19 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string, vector<int> index_list) {
+    string answer = "";
+    for (auto i : index_list) {
+        answer += my_string[i];
+    }
+    return answer;
+}

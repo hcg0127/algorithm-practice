7642dce637200f2a9070d10f26139cb005ec4c9b
Eunji Kim <kej431003@gmail.com>
2025-10-30 19:18:43 +0900

    [level 0] Title: 세로 읽기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
@@ -5,10 +5,9 @@ using namespace std;
 
 string solution(string my_string, int m, int c) {
     string answer = "";
-    int ss = my_string.size(), i=c-1;
-    while (i<ss) {
+    int ss = my_string.size();
+    for (int i=c-1; i<ss; i+=m) {
         answer += my_string[i];
-        i+=m;
     }
     return answer;
 }

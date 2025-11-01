7990021b58e85ae53313c4fc9466389bd2487d0f
Eunji Kim <kej431003@gmail.com>
2025-11-01 22:07:17 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> numbers, int n) {
+    int answer = 0;
+    for (auto i : numbers) {
+        answer += i;
+        if (answer > n) break;
+    }
+    return answer;
+}

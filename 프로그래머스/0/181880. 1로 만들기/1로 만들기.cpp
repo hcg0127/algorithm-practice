72ddcfcd0e8ac70d064ea7087d0de122a748de5b
Eunji Kim <kej431003@gmail.com>
2025-11-02 18:12:22 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
@@ -7,11 +7,7 @@ int memo[31],answer;
 int solution(vector<int> num_list) {
     memo[2] = 1;
     for (int i=3; i<=30; i++) {
-        if (i%2) {
-            memo[i] = memo[(i-1)/2] + 1;
-        } else {
-            memo[i] = memo[i/2] + 1;
-        }
+        memo[i] = memo[i/2] + 1;
     }
     for (auto i : num_list) {
         answer += memo[i];

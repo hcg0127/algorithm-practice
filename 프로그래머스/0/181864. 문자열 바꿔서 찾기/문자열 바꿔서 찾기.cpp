03529f7e0f43bd7e111583d0f00840668bbd6709
Eunji Kim <kej431003@gmail.com>
2025-11-03 10:24:23 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    for (auto &c : myString) {
+        if (c=='A') c='B';
+        else if (c=='B') c='A';
+    }
+    return myString.find(pat) != string::npos;
+}

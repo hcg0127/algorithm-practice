d7e1af56cdafa05d19f0af1e73624f4d0c14600d
Eunji Kim <kej431003@gmail.com>
2025-11-04 17:36:41 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 0.43 ms, Memory: 4.28 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.cpp
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.cpp
@@ -4,19 +4,13 @@ using namespace std;
 bool flag[100001];
 
 vector<int> solution(vector<int> arr, int k) {
-    vector<int> answer, s;
+    vector<int> s;
     for (auto i : arr) {
         if (!flag[i]) {
             s.push_back(i);
             flag[i] = 1;
         }
     }
-    if (s.size()<=k) {
-        answer = s;
-        answer.insert(answer.end(), k-s.size(), -1);
-    }
-    else {
-        for (int i=0; i<k; i++) answer.push_back(s[i]);
-    }
-    return answer;
+    s.resize(k,-1);
+    return s;
 }

6e3fd1b1a199c73e44dabcc3d58ad3b845fa084b
Eunji Kim <kej431003@gmail.com>
2025-11-05 23:16:58 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.28 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -3,10 +3,8 @@
 using namespace std;
 
 long long solution(long long n) {
-    long long answer = 0;
     string s = to_string(n);
     sort(s.begin(),s.end());
     reverse(s.begin(),s.end());
-    for (auto c : s) answer = answer*10 + (c-'0');
-    return answer;
+    return stoll(s);
 }

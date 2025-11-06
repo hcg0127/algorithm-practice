10466bae4b895cff4c226ef2d0c930c78f60b670
Eunji Kim <kej431003@gmail.com>
2025-11-06 13:51:01 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -3,13 +3,5 @@
 using namespace std;
 
 string solution(string n_str) {
-    bool flag = 0;
-    string answer = "";
-    for (auto i : n_str) {
-        if (i!='0') {
-            if (!flag) flag = 1;
-            answer.push_back(i);
-        } else if (i=='0' && flag) answer.push_back(i);
-    }
-    return answer;
+    return to_string(stoi(n_str));
 }

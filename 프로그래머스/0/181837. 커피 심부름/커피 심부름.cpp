eee7c78a98935ab6003c4705f6bb9a1a8bf4803c
Eunji Kim <kej431003@gmail.com>
2025-11-07 09:23:32 +0900

    [level 0] Title: 커피 심부름, Time: 0.05 ms, Memory: 4.43 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.cpp
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int solution(vector<string> order) {
     int answer = 0, ame = 0, caf = 0;
     for (auto s : order) {
-        if (s.find("americano")!=string::npos || s.compare("anything")==0) ame++;
-        else if (s.find("cafelatte")!=string::npos) caf++;
+        if (s.find("cafelatte")!=string::npos) caf++;
+        else ame++;
     }
     return ame * 4500 + caf * 5000;
 }

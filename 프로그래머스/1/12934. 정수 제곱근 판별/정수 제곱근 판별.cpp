9933b48099343f328c717a3020c5942ebd4445ca
Eunji Kim <kej431003@gmail.com>
2025-11-08 22:02:06 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    cout << isdigit(sqrt(n));
+    return (int)sqrt(n) == sqrt(n) ? pow(sqrt(n)+1,2) : -1;
+}

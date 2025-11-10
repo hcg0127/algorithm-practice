56651ceceb61de932bba12391ec957bfa80e217e
Eunji Kim <kej431003@gmail.com>
2025-11-10 20:49:00 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -1,18 +1,9 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int solution(int n) {
-    string tmp = "";
-    while (n>2) {
-        tmp += to_string(n%3);
-        n/=3;
-    }
-    tmp += to_string(n);
-    int ss = tmp.size(), answer=0;
-    for (auto i : tmp) {
-        answer += (i-'0') * pow(3,ss-1);
-        ss--;
-    }
+    int answer = 0;
     return answer;
 }

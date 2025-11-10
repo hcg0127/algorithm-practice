c3674f82758e19a72d4240c37040e446f4a123ef
Eunji Kim <kej431003@gmail.com>
2025-11-10 10:37:41 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 2.98 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    for (int i=n; i>0; i--) {
+        for (int j=i; j<=n; j++) cout << '*';
+        cout << '\n';
+    }
+    return 0;
+}

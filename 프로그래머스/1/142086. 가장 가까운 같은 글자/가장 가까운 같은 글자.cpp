0f3112a30422bab4e9fff65f7b94a90bf2caceb4
Eunji Kim <kej431003@gmail.com>
2025-11-12 15:32:14 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.30 ms, Memory: 5.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+bool al[26];
+int pos[26];
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    int vs = s.size();
+    for (int i=0; i<vs; i++) {
+        char c = s[i];
+        if (!al[c-'a']) {
+            answer.push_back(-1);
+            al[c-'a']=1;
+            pos[c-'a']=i;
+        } else {
+            answer.push_back(i-pos[c-'a']);
+            pos[c-'a']=i;
+        }
+    }
+    return answer;
+}

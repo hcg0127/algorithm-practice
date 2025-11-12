8ba93e37299f20ff825fb77f34b453792c389b8d
Eunji Kim <kej431003@gmail.com>
2025-11-12 16:38:44 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.07 ms, Memory: 4.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int num;
+
+bool cmp(string a, string b) {
+    if (a[num]==b[num]) return a<b;
+    else return a[num]<b[num];
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    num = n;
+    sort(strings.begin(),strings.end(),cmp);
+    return strings;
+}

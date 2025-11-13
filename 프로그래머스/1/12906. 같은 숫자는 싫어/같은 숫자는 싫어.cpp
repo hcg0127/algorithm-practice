c3474f6bd794f587ae6837b80b62e0805c9f1510
Eunji Kim <kej431003@gmail.com>
2025-11-13 13:55:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 107.72 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -4,18 +4,6 @@ using namespace std;
 
 vector<int> solution(vector<int> arr) 
 {
-    vector<int> answer;
-    stack<int> s;
-
-    for (auto i : arr) {
-        if (s.empty()) {
-            s.push(i);
-            answer.push_back(i);
-        } else if (s.top()!=i) {
-            s.push(i);
-            answer.push_back(i);
-        }
-    }
-
-    return answer;
+    arr.erase(unique(arr.begin(),arr.end()),arr.end());
+    return arr;
 }

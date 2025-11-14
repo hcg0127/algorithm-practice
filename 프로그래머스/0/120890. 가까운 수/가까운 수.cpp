c579d9cfa32f22e8af2bd6bf83e4158346cd64de
Eunji Kim <kej431003@gmail.com>
2025-11-14 19:48:25 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.cpp
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.cpp
@@ -7,6 +7,6 @@ int solution(vector<int> array, int n) {
     sort(array.begin(),array.end());
     auto tmp = lower_bound(array.begin(),array.end(),n) - array.begin();
     if (vs==tmp) return array[vs-1];
-    else if (array[tmp]-n == n-array[tmp-1]) return array[tmp-1];
+    else if (array[tmp]-n > n-array[tmp-1]) return array[tmp-1];
     else return array[tmp];
 }

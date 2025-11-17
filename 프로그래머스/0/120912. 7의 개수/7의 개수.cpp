3b8b624b3ff492dd7e6c2d24738fe8baa7029f85
Eunji Kim <kej431003@gmail.com>
2025-11-17 19:16:29 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    for (auto i : array) {
+        while (i) {
+            if (i%10==7) answer++;
+            i/=10;
+        }
+    }
+    return answer;
+}

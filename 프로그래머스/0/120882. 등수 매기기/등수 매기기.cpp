1d1c1c58a8486219ef5b32193ec7b3889504a1c4
Eunji Kim <kej431003@gmail.com>
2025-11-20 11:19:54 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define pdi pair<double,int>
+
+using namespace std;
+
+bool cmp1(pdi a, pdi b) {
+    return a.first>b.first;
+}
+
+bool cmp2(pdi a, pdi b) {
+    return a.second<b.second;
+}
+
+vector<int> solution(vector<vector<int>> score) {
+    vector<pdi> tmp;
+    int vs = score.size();
+    for (int i=0; i<vs; i++) {
+        tmp.push_back({(double)(score[i][0]+score[i][1])/2, i});
+    }
+    sort(tmp.begin(),tmp.end(),cmp1);
+    map<double,int,greater<double>> m;
+    for (auto v : tmp) {
+        m[v.first]++;
+    }
+    int idx=0,rank=1;
+    for (auto p : m) {
+        if (p.second==1) {
+            tmp[idx++].first = rank++;
+        } else {
+            int ps = p.second;
+            while (ps--) {
+                tmp[idx].first = rank;
+                idx++;
+            }
+            rank += p.second;
+        }
+    }
+    sort(tmp.begin(),tmp.end(),cmp2);
+    vector<int> answer;
+    for (auto p : tmp) {
+        answer.push_back(p.first);
+    }
+    return answer;
+}

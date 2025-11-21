ed22595f6b28bef9522d4bfec6124348456a9890
Eunji Kim <kej431003@gmail.com>
2025-11-22 00:08:49 +0900

    [level 0] Title: A로 B 만들기, Time: 0.07 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(string before, string after) {
+    sort(before.begin(),before.end());
+    sort(after.begin(),after.end());
+    return before.compare(after)==0;
+}

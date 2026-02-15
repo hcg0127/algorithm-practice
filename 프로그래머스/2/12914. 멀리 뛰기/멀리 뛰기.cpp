0734a3b2089e51b9785fef1aabe9a14c02255af7
Eunji Kim <kej431003@gmail.com>
2026-02-15 14:04:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long arr[2001];
+
+long long solution(int n) {
+    long long answer = 0;
+    arr[1] = 1;
+    arr[2] = 2;
+    for (int i=3; i<=n; i++) {
+        arr[i] = (arr[i-1]%1234567 + arr[i-2]%1234567)%1234567;
+    }
+    
+    return arr[n];
+}
